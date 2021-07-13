5dccc158531c34e3c91e85663338d9436083213c
Parkduksung <duksung1234@naver.com>
2021-07-13 23:27:00 +0900

    [Fragment] FrameLayout 사용.

--- app/src/main/java/com/example/dev_sk/fragment/FragmentActivity.kt
+++ app/src/main/java/com/example/dev_sk/fragment/FragmentActivity.kt
@@ -1,4 +1,17 @@
 package com.example.dev_sk.fragment
 
-class FragmentActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dev_sk.R
+
+class FragmentActivity : AppCompatActivity(R.layout.activity_fragment_exam) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.container, AMainFragment())
+        }.commit()
+
+    }
 }
