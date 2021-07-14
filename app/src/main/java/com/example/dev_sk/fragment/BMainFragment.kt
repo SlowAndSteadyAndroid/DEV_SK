a3316eac73418392c3af345ba5d76de3ec8ed758
Parkduksung <duksung1234@naver.com>
2021-07-14 20:25:11 +0900

    [Fragment] A,B 생성.

--- app/src/main/java/com/example/dev_sk/fragment/BMainFragment.kt
+++ app/src/main/java/com/example/dev_sk/fragment/BMainFragment.kt
@@ -1,4 +1,68 @@
 package com.example.dev_sk.fragment
 
-class BMainFragment {
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.dev_sk.R
+
+class BMainFragment : Fragment(R.layout.fragment_b_main) {
+
+
+    override fun onAttach(context: Context) {
+        Log.d(TAG, "onAttach")
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d(TAG, "onViewCreated")
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    override fun onStart() {
+        Log.d(TAG, "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d(TAG, "onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.d(TAG, "onDetach")
+        super.onDetach()
+    }
+
+    companion object {
+        private const val TAG = "결과 BMainFragment"
+    }
 }
