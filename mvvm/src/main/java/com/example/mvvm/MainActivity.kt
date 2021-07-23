0134e3b04283df0603fb38a9701dfd371f3f227e
Parkduksung <duksung1234@naver.com>
2021-07-23 13:17:16 +0900

    [MVVM] dataBinding 기본을 이용해서 간단한 덧셈 적용.

--- mvvm/src/main/java/com/example/mvvm/MainActivity.kt
+++ mvvm/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+        with(binding) {
+            buttonResult.setOnClickListener {
+                tvResult.text =
+                    (etInputA.text.toString().toInt() + etInputB.text.toString().toInt()).toString()
+            }
+        }
     }
 }
