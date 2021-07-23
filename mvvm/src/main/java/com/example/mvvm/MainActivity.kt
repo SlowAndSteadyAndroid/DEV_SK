21efc7a0585feb24c465853f0f625c825689ba9f
Parkduksung <duksung1234@naver.com>
2021-07-23 13:26:08 +0900

    [MVVM] VM 생성 , two-way binding 을 이용하여 editText 의 변화 감지.

--- mvvm/src/main/java/com/example/mvvm/MainActivity.kt
+++ mvvm/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.mvvm.databinding.ActivityMainBinding
 
@@ -9,10 +10,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val mainViewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = mainViewModel
         setContentView(binding.root)
+
+
+        with(mainViewModel) {
+
+            inputA.observe(this@MainActivity) {
+                Log.d("결과", "InputA : $it")
+            }
+
+            inputB.observe(this@MainActivity) {
+                Log.d("결과", "InputB : $it")
+            }
+
+        }
 
         with(binding) {
             buttonResult.setOnClickListener {
---
+++ mvvm/src/main/java/com/example/mvvm/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.mvvm
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    val inputA = MutableLiveData<String>()
+    val inputB = MutableLiveData<String>()
+
+
+
+
+}
