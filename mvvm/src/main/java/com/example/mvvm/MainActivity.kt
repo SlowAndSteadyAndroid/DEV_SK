d9053761e74dcdc407b43bc033d1eda84e59f46e
Parkduksung <duksung1234@naver.com>
2021-07-27 19:55:33 +0900

    [refactor-MVVM] MVVM 에 맞게 적용. sealed class 적극 활용. 코루틴 사용.

--- mvvm/src/main/java/com/example/mvvm/MainActivity.kt
+++ mvvm/src/main/java/com/example/mvvm/MainActivity.kt
@@ -1,26 +1,32 @@
 package com.example.mvvm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import com.example.mvvm.data.repo.MainRepositoryImpl
+import com.example.mvvm.data.source.local.LocalDataSourceImpl
 import com.example.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val mainViewModel = MainViewModel()
+    private val mainViewModel =
+        MainViewModel(MainRepositoryImpl.getInstance(LocalDataSourceImpl.getInstance()))
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         binding.viewModel = mainViewModel
         setContentView(binding.root)
 
-        mainViewModel.mainViewStateLiveData.observe(this) {
-            when (it) {
+        mainViewModel.mainViewStateLiveData.observe(this) { viewState ->
+            when (viewState) {
 
                 MainViewModel.MainViewState.StartOperation -> {
                     binding.progressbar.isVisible = true
@@ -35,9 +41,15 @@ class MainActivity : AppCompatActivity() {
                         (binding.etInputA.text.toString().toInt() + binding.etInputB.text.toString()
                             .toInt()).toString()
                 }
+                MainViewModel.MainViewState.RouteSub -> {
+                    startActivity(Intent(this@MainActivity, SubActivity::class.java))
+                }
+
+                is MainViewModel.MainViewState.GetLocalData -> {
+                    binding.tvMain.text = viewState.data
+                }
             }
 
         }
-
     }
 }
--- mvvm/src/main/java/com/example/mvvm/MainViewModel.kt
+++ mvvm/src/main/java/com/example/mvvm/MainViewModel.kt
@@ -3,28 +3,36 @@ package com.example.mvvm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.mvvm.data.repo.MainRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
-
+class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
 
     private val _mainViewStateLiveData = MutableLiveData<MainViewState>()
     val mainViewStateLiveData: LiveData<MainViewState> = _mainViewStateLiveData
 
     fun operation() {
         CoroutineScope(Dispatchers.Main).launch {
+            getLocalData()
 
             startOperation()
 
-            delay(2000)
+            delay(1000)
 
             operationPlus()
 
             endOperation()
 
+            delay(1000)
+
+            updateLocalData()
+            getLocalData()
+
+            delay(1000)
+            routeSubActivity()
         }
     }
 
@@ -41,12 +49,23 @@ class MainViewModel : ViewModel() {
         _mainViewStateLiveData.value = MainViewState.OperationPlus
     }
 
+    private fun routeSubActivity() {
+        _mainViewStateLiveData.value = MainViewState.RouteSub
+    }
+
+    private fun updateLocalData() {
+        mainRepository.setLocalData("updateData")
+    }
+
+    private fun getLocalData() {
+        _mainViewStateLiveData.value = MainViewState.GetLocalData(mainRepository.getLocalData())
+    }
 
     sealed class MainViewState {
         object StartOperation : MainViewState()
         object EndOperation : MainViewState()
-        object OperationPlus : MainViewState()
+        object OperationPlus : MainViewState() // dataClass
+        object RouteSub : MainViewState()
+        data class GetLocalData(val data: String) : MainViewState()
     }
-
 }
-
