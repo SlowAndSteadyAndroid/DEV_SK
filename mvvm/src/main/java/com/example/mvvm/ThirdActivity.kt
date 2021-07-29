1a227835b389ab4e832b4cb1c1baa4d9bd6ee492
Parkduksung <duksung1234@naver.com>
2021-07-29 21:43:33 +0900

    추상클래스 생성. 차근차근 공통적인거 찾기 3단계 - 새로운 액티비티를 만드는 작업(응용)

---
+++ mvvm/src/main/java/com/example/mvvm/ThirdActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvm
+
+import android.os.Bundle
+import com.example.mvvm.base.BaseActivity
+import com.example.mvvm.databinding.ActivityThirdBinding
+
+class ThirdActivity : BaseActivity<ActivityThirdBinding>(R.layout.activity_third) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
